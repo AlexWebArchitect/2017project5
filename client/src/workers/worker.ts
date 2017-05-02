1ccd43f73586106b6ad9baab67cdb7b3242a7523
garygolf <garygolf@mail.ru>
2017-05-02 10:37:35 +0300

    add api

--- client/src/workers/worker.ts
+++ client/src/workers/worker.ts
@@ -1,20 +1,25 @@
 import * as Actions from '../constants/actions'
+import axios from 'axios'
+import * as api from './api'
 
 interface State {
+    posts: Array<PostListItem>
 }
 
 const state: State = {
+    posts: []
 }
 
 function onMessage(event) {
     const {type, payload}: Action = event.data
     switch(type){
-       
         case Actions.LOAD_LAST_POSTS:
-            self.postMessage.apply(null, [{type: Actions.LOAD_LAST_POSTS, payload: []}])
+            api.loadLastPosts()
+            .then(payload => self.postMessage.apply(null, [{type: Actions.LOAD_LAST_POSTS, payload}]))
             break
         default :
     }
 }
 
 self.addEventListener('message', onMessage)
+
